372ab9d73d2b2822f09737b01f0db126d8adab30
Chethana N P <chethanaprakash.np@gmail.com>
2023-06-21 22:22:09 +0530

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Cumulations Technologies Private Limited on 21/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
